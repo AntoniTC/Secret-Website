3aeca4f7738fc806fe5e8c5c0cff9051700f1486
AntoniTC <46378064+AntoniTC@users.noreply.github.com>
2026-02-20 23:07:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const button = document.getElementById("btn");
 const password = document.getElementById("password");
 const image = document.getElementById("img");
 
-const passwords = {"eevee": "sushi.png"}
+const passwords = {
+    "eevee" : "sushi.png",
+    "1410" : "stardew.png"
+}
 
 button.addEventListener("click", () => {
-    const password_text = password.value;
+    const password_text = password.value.toLowerCase();
     if (password_text in passwords){
         const src = passwords[password_text];
         alert("Correct password found.")
